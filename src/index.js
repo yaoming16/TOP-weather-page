708c9f36ea660b2c65ea7f8cf1adaec87e6faba8
Pablo Perez <pabloperezzz16@gmail.com>
2025-11-07 19:22:38 +0000

    Added fetch to get latitud and longitud from city name Need to check getFormattedLocationData function

--- src/index.js
+++ src/index.js
@@ -9,25 +9,17 @@ const params = {
 	"timezone": "auto",
 };
 
-async function fetchData(url, params) {
-    try {
-        const response = await fetchWeatherApi(url, params);
-        return response;
+async function getFormattedWeatherData(url, params) {
+    let weatherDataResponse;
+    try { 
+        if (!url || !params) throw new Error("URL or parameters are missing.");
+        weatherDataResponse = await fetchWeatherApi(url, params);
     } catch (error) {
         console.error("Error fetching weather data:", error);
         return null;
     }
-}
-
-async function getFormattedWeatherData(url, params) {
-    let weatherDataResponse = await fetchData(url, params);
     
-    if (weatherDataResponse === null) {
-        console.log("Failed to retrieve weather data.");
-        return -1;
-    } else {
-        weatherDataResponse = weatherDataResponse[0];
-    }
+    weatherDataResponse = weatherDataResponse[0];
     
     const utcOffsetSeconds = weatherDataResponse.utcOffsetSeconds();
     const current = weatherDataResponse.current();
@@ -42,9 +34,40 @@ async function getFormattedWeatherData(url, params) {
     return weatherData;
 }
     
-    const url = "https://api.open-meteo.com/v1/forecast";
-    const weatherData = await getFormattedWeatherData(url, params);
-    if (weatherData !== -1) {
-        console.log(weatherData);
+const weatherUrl = "https://api.open-meteo.com/v1/forecast";
+const weatherData = await getFormattedWeatherData(weatherUrl, params);
+if (weatherData !== null) {
+    console.log(weatherData);
+}
+
+let city = "Montevideo";
+const locationUrl = `https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=1&language=en&format=json`;
+
+
+// Need to check this function 
+async function getFormattedLocationData(url, userSelectedCity) {
+
+    try {
+        let locationDataResponse = await fetch(url);
+        locationDataResponse = await locationDataResponse.json();
+        locationDataResponse = locationDataResponse.results[0];
+        if (!locationDataResponse) throw new Error("Location not found");
+        return {
+            latitude: locationDataResponse.latitude,
+            longitude: locationDataResponse.longitude,
+            name: locationDataResponse.name,
+        };
+    } catch (error) {
+        console.error("Error fetching location data:", error);
+        return null;
     }
+
+}
+
+
+
+
+
+
 //https://open-meteo.com/en/docs?bounding_box=null,null,null,null&latitude=-34.9033&longitude=-56.1882&timezone=auto&hourly=&current=temperature_2m,apparent_temperature,precipitation
+//https://open-meteo.com/en/docs/geocoding-api
